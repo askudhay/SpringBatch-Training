e24c6ddffdd8572066f2a313851c4e9fa71932d8
Udhay <systemudhaya@gmail.com>
2017-09-16 23:17:39 -0400

    Example 6

---
+++ CustomMetaDataTablesEx/src/main/java/com/spring/ex/BatchStepOne.java
@@ -0,0 +1,16 @@
+package com.spring.ex;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class BatchStepOne implements Tasklet{
+	
+	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1)
+			throws Exception {
+		System.out.println("Inside IntialiseStep");
+		return RepeatStatus.FINISHED;
+	}
+
+}
